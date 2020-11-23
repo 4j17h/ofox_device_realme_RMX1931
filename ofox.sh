e3413db8b798ad5b921415929712274204b82240
4j17h <7hou74m4j17h@gmail.com>
2020-11-23 17:04:05 +0530

    RMX1931: Script: Update version to R11.0_1

--- ofox.sh
+++ ofox.sh
@@ -7,7 +7,7 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_ADVANCED_SECURITY="1"	
 export FOX_ADVANCED_STOCK_REPLACE="1"
 export FOX_BUILD_FULL_KERNEL_SOURCES="1"
-export FOX_BUILD_TYPE="Beta"	
+export FOX_BUILD_TYPE="Stable"	
 export FOX_DELETE_AROMAFM="1"
 export FOX_R11="1"
 export FOX_REPLACE_BUSYBOX_PS="1"
@@ -15,7 +15,7 @@ export FOX_RESET_SETTINGS="1"
 export FOX_USE_BASH_SHELL="1"
 export FOX_USE_NANO_EDITOR="1"
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
-export FOX_VERSION="R11.0"	
+export FOX_VERSION="R11.0_1"
 export LC_ALL="C"
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
 export OF_FLASHLIGHT_ENABLE="1"
@@ -30,7 +30,7 @@ export OF_TARGET_DEVICES="RMX1931,RMX1931CN,RMX1931L1"
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
 export OF_USE_NEW_MAGISKBOOT="1"
 export TARGET_ARCH="arm64"
-export TW_DEVICE_VERSION="R11.0"
+export TW_DEVICE_VERSION="R11.0_1"
 export USE_CCACHE="1"
 
 	# lzma
@@ -49,7 +49,7 @@ do_build() {
 
   lunch omni_"$TARGET_DEVICE"-eng
 
-  mka recoveryimage -j$(nproc --all) && cd device/realme/RMX1931
+  time mka recoveryimage && cd device/realme/RMX1931
 }
 
 # --- main --- #
